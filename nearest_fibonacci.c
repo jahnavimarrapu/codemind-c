a275df167c79bd043ee50afc48dc193d3e8f5970
marrapu jahnavi kanaka mahalakshmi <120192715+jahnavimarrapu@users.noreply.github.com>
2023-05-03 15:14:15 +0530

    #include<stdio.h> int main() {     int a=0,b=1,c=0,n;     scanf("%d",&n);     while(n>c)     {         c=a+b;         a=b;         b=c;     }     if(n-a<b-n)     {         printf("%d ",a);     }     else if(n-a==b-n)     {         printf("%d %d",a,b);     }     else     {         printf("%d ",b);     } }

---
+++ nearest_fibonacci.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+int main()
+{
+    int a=0,b=1,c=0,n;
+    scanf("%d",&n);
+    while(n>c)
+    {
+        c=a+b;
+        a=b;
+        b=c;
+    }
+    if(n-a<b-n)
+    {
+        printf("%d ",a);
+    }
+    else if(n-a==b-n)
+    {
+        printf("%d %d",a,b);
+    }
+    else
+    {
+        printf("%d ",b);
+    }
+}
+

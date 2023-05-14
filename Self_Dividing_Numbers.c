f1921a6b96e2d8e97512391b77fa77981f512bd4
marrapu jahnavi kanaka mahalakshmi <120192715+jahnavimarrapu@users.noreply.github.com>
2023-05-14 18:56:02 +0530

    #include<stdio.h> int main() {     int a,b,q,c=0,tc=0,r;     scanf("%d%d",&a,&b);     int i,j;     for(i=a;i<=b;i++)     {         q=i;         c=0;         tc=0;         while(q!=0)         {             c++;             r=q%10;             if(r!=0 && i%r==0)             {                 tc++;             }             q=q/10;         }         if(c==tc)         printf("%d ",i);     } }

--- Self_Dividing_Numbers.c
+++ Self_Dividing_Numbers.c
@@ -1,29 +1,25 @@
 #include<stdio.h>
 int main()
 {
-    int a,b;
+    int a,b,q,c=0,tc=0,r;
     scanf("%d%d",&a,&b);
-    int  i;
+    int i,j;
     for(i=a;i<=b;i++)
     {
-        int q,c,tc;
         q=i;
         c=0;
         tc=0;
         while(q!=0)
         {
-            int r;
-            c=c+1;
+            c++;
             r=q%10;
-            if(r!=0&&i%r==0)
+            if(r!=0 && i%r==0)
             {
-                tc=tc+1;
+                tc++;
             }
             q=q/10;
         }
         if(c==tc)
-        {
-            printf("%d ",i);
-        }
+        printf("%d ",i);
     }
 }
